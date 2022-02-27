339b52bd180a03ac0379f663259de47902ba3bf2
Thiago Costa <thiago14abr@gmail.com>
2022-02-27 11:47:17 -0300

    Validating env schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigValidationSchema } from './config.schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: [`.env.stage.${process.env.STAGE}`] }),
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: ConfigValidationSchema,
+    }),
     TasksModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
